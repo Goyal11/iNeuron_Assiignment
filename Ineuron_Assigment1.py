b69a90a8deb3f040cd0b3ce067520d81645a1471
Goyal11 <94910258+Goyal11@users.noreply.github.com>
2022-05-19 22:21:43 +0530

    Add files via upload

---
+++ Ineuron_Assigment1.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Q1
+ Expressions ARE :
+    *,
+    -,
+    (,)
+    +.
+    
+    Values ARE :
+    'Hello',
+    -87.8,
+    6.
+
+
+# In[ ]:
+
+
+#Q2.
+
+String is the Collection of Words and digits , must be enclosed within Literals.
+
+Wheareas 
+
+Variables is a used to assign a Value and to reserve a Space in memory.
+
+
+#     #Q3.
+#     
+#     The Three types of Data types are :
+#     1. Strings.
+#     2. Integers
+#     3. Booleans.
+# 
+
+# In[ ]:
+
+
+# Q4.
+Expression are mathematical operators.
+Expression are used to perform mathematical operations between two operants.
+
+
+# In[ ]:
+
+
+#Q5.
+
+like spam = 10
+will assign a value 10 to the variable "like spam".
+
+whereas
+
+Expression is used to perform mathematical operation between two operants or Variable.
+
+
+# In[1]:
+
+
+#Q6.
+
+bacon = 23
+bacon +1
+
+
+# In[2]:
+
+
+#Q7.Part1
+
+'spam'+ 'spamspam'
+
+
+# In[3]:
+
+
+#Q7. Part2.
+
+'spam'*3
+
+
+# In[ ]:
+
+
+#Q8. 
+
+Variable name always start with a String , not with an integer in python.
+Thats why eggs is correct variable name, not a 100.
+
+
+# In[ ]:
+
+
+#Q9.
+
+The three functions are:
+    int(value),
+    str(Value),
+    float(Value).
+
+
+# In[ ]:
+
+
+#Q10.
+I have eaten ' + 99 + ' burritos.'
+In this , 'I have eaten 'is the String and 99 is an integer and ' burritos. ' is also a String.
+We cannot concatinate two different datatypes together that why it is throwing an error.
+
+We can fix this error by typecasting an integer value(99) into a String value and then able to perform concatination Operation.
+
+'I have eaten'+ '99' + 'burritos.'
+
