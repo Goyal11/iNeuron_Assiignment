518c8d95529694f1a39cef787e8b5a70f317e5e2
Goyal11 <94910258+Goyal11@users.noreply.github.com>
2022-05-20 22:01:41 +0530

    Add files via upload

---
+++ Assissment_2.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Q1.
+
+The two types of Boolean data types are:
+    1. True 
+    2. False
+    
+    We can write them as a bool
+
+
+# In[ ]:
+
+
+#Q2.
+
+The three different types of boolean operators are:
+    1. and
+    2. or
+    3. not
+
+
+# In[ ]:
+
+
+#Q3.
+Truth Table:
+    1. True and True .... True
+    2. True and False .... False
+    3. False and True .... False
+    4. False and False .... False
+    5. True or True.... True
+    6. True or False....True
+    7. False or True.....True
+    8. False or False .... False
+
+
+# In[ ]:
+
+
+#Q4.
+
+(5 > 4) and (3 == 5).....False
+not (5 > 4)......False
+(5 > 4) or (3 == 5)......True
+not ((5 > 4) or (3 == 5))......False
+(True and True) and (True == False).....False
+
+(not False) or (not True).......True
+
+
+# In[ ]:
+
+
+#Q5.
+
+The six types of relational operators:
+    1. ==
+    2. !=
+    3. >=
+    4. <=
+    5. >
+    6. <
+
+
+# In[ ]:
+
+
+#Q6.
+
+= will assign a value to a variable .
+Whereas 
+== will check whether value store in a variable will equal to other variable or not.
+
+
+# In[ ]:
+
+
+#Q7.
+
+The three blocks in this code are:
+    1. If
+    2. elif
+    3. else
+
+
+# In[8]:
+
+
+#Q8.
+
+spam = int(input("Enter the Value :"))
+if spam == 1:
+    print('Hello')
+elif spam ==2:
+    print('Howdy')
+else:
+    print('Greetings!')
+
+
+# In[ ]:
+
+
+#Q9.
+
+If our programme stuck in the endless loop, we have to press Ctrl + C to kill the programme where 'C' stand for Cancel.
+
+
+# In[ ]:
+
+
+#10.
+
+Break .... By Using 'Break' we can came out of a loop.
+
+Whereas
+
+Continue.... By using 'Continue' we can keep on checking the condition in the loop until the loop end.
+
+
+# In[ ]:
+
+
+#11.
+
+In  for loop range(10), range(0,10) and range(0,10,1) there is no difference , all will gave the same output.
+All will numbers from 0 to 9 as a output.
+
+
+# In[9]:
+
+
+#12.
+
+# Using For loop
+for i in range(1,11):
+    print(i)
+
+
+# In[10]:
+
+
+#12.
+#Using While loop
+i =1
+while i < 11:
+    print(i)
+    i = i+1
+
+
+# In[ ]:
+
+
+#13
+
