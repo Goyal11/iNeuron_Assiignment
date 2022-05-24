aff0b3f99b24742dbc8be540d55abf5cd7c25d25
Goyal11 <94910258+Goyal11@users.noreply.github.com>
2022-05-24 14:58:21 +0530

    Add files via upload

---
+++ Ineuron_Assigment4(1).py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Q1.
+
+[] is a list.
+
+
+# In[1]:
+
+
+#Q2.
+spam =[2,4,6,8,10]
+spam.insert(3,'hello')
+spam
+
+
+# In[2]:
+
+
+#Q3.
+
+spam = ['a','b','c','d']
+spam[int(int('3' * 2) / 11)]
+
+
+# In[3]:
+
+
+#Q4.
+
+spam[-1]
+
+
+# In[4]:
+
+
+#Q5.
+
+spam[:2]
+
+
+# In[10]:
+
+
+#Q6.
+
+
+bacon = [3.14, 'cat', 11, 'cat', True] 
+
+bacon.index('cat')
+
+
+# In[11]:
+
+
+#Q7.
+
+bacon.append(99)
+bacon
+
+
+# In[12]:
+
+
+#Q8.
+
+bacon.remove('cat')
+bacon
+
+
+# In[ ]:
+
+
+#Q9.
+
+# List Concatenation operators
+
+List Concatenation operators are operators are used to add two different list together and create a new list.
+
+# List Replication operators.
+ List Replication operators are used to make changes in the existing list.
+
+
+# In[ ]:
+
+
+#Q10.
+
+append() will add a new element to the list at the end of the existing list.
+
+Whereas
+
+insert() will add a new element to the particular index in a existing list and all the other elements shift ahead.
+
+
+# In[ ]:
+
+
+#Q11.
+
+The two methods for removing a items from a list are:
+    
+    1. remove()
+    2. clear()
+
+
+# In[ ]:
+
+
+#Q12.
+
+List and String Values are identical in a way both can allocate a index to the values store in a memory.
+
+
+# In[ ]:
+
+
+#Q13.
+
+Tuples are immuteable.
+
+Whereas 
+
+List are muteable.
+
+
+# In[22]:
+
+
+#Q14.
+
+tup1 =(42)
+
+
+# In[29]:
+
+
+#Q15.
+
+# We can get a Tuple Values into list form by typecasting Tuple values into list.
+# For example:
+
+tup1 = (1,2,3)
+list(tup1)
+
+
+
+# In[4]:
+
+
+#Q15. 
+# We can get a List Values into a tuple form by  typecasting List values into tuple.
+#  For example:
+
+L1 = [1,2,3]
+tuple(L1)
+
+
+# In[40]:
+
+
+#Q16. 
+   
+Variables that contains list values are not necessarily a list themselves , it can be any datatypes like tuple, set and dict.
+   
+      
+   
+
+
+# In[ ]:
+
+
+#Q17.
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
