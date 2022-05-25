9d4e95acdffb18b115d7768e28430a8e3779ef1c
Goyal11 <94910258+Goyal11@users.noreply.github.com>
2022-05-25 10:21:27 +0530

    Add files via upload

---
+++ Ineuron Program _Assig1.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#Q1.
+
+a = "Hello python"
+print(a)
+
+
+# In[4]:
+
+
+#Q2.
+N1 =int(input("Enter the NUM1 : "))
+N2 =int(input("Enter the NUM2 : "))
+
+ADD = N1 + N2
+
+
+
+ADD
+
+
+# In[5]:
+
+
+#Q2.
+
+n1 =int(input("Enter the NUM1 : "))
+n2 =int(input("Enter the NUM2 : "))
+
+DIV = n1/n2
+
+DIV
+
+
+# In[6]:
+
+
+#Q3.
+
+B = int(input("Enter the base of triangle: "))
+H = int(input("Enter the height of triangle: "))
+
+A = 0.5*B*H
+A
+
+
+# In[17]:
+
+
+#Q4.
+
+a = int(input("Enter the value: "))
+b = int(input("Enter the value: "))
+
+c = a 
+a = b 
+b = c 
+
+print("Value of a after Swaping: {}".format(a))
+print("Value of b after Swaping: {}" .format(b))
+
+
+# In[21]:
+
+
+#Q5.
+import random
+print(random.randint(0,9))
+
+
+# In[ ]:
+
+
+
+
