66fc10d4d09673261fcd60accb2371a4c0cb1733
Goyal11 <94910258+Goyal11@users.noreply.github.com>
2022-06-02 17:27:13 +0530

    Add files via upload

---
+++ 3ASSSINMENT.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Q1.
+
+Functions are used so that we use a same logic again and again  by calling a Function Name without writing 
+a code again and again.
+
+
+# In[ ]:
+
+
+#Q2.
+
+Code in a function run only when we call a function name.
+
+
+# In[ ]:
+
+
+#Q3. 
+def functionName() creates a function.
+
+
+# In[ ]:
+
+
+#Q4.
+    Function can hold only the defination(logic) of the function.
+    
+    Whereas
+    
+    Function call only needs an argument and trigger a function to run.
+
+
+# In[ ]:
+
+
+#Q5.
+
+We have to create one global or one local Variable while Function is called.
+
+
+# In[ ]:
+
+
+#Q6.
+
+When function call return , the local scope are destroyed and all the variables are terminated.
+
+
+# In[ ]:
+
+
+#Q7.
+
+return will give the value after execution of logic define in the function. Yes, we will return value in an  expression .
+
+
+# In[8]:
+
+
+#Q8.
+
+If a function does not have a return , and we call a function , it will not return any value to us.
+       
+
+
+# In[10]:
+
+
+#Q9.
+
+We define function variable as a global variable , when we declare a variable outside a function defination and mention global and then write a variable name.
+
+
+# In[12]:
+
+
+#Q10.
+Data type of NONE is None Type.
+
+
+# In[ ]:
+
+
+#11.
+areallyourpetsnamederic
+sentence import areallyourpetsnamederic imports a module name areallyourpetsnamederic.
+
+
+# In[ ]:
+
+
+#12.
+
+We will call it as a spam.bacon() .
+
+
+# In[ ]:
+
+
+#13.
+
+If a program encounter an error we can put the program in try Clause to Save the program from Crashing.
+
+
+# In[ ]:
+
+
+#14.
+
+ Try Clause save the program from crashing when error occur.
+ 
+ Program goes to expect Clause when there is an error in the program after excecution.
+
