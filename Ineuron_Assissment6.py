df6b0fbda1b299ddc9d1da19d8ce9ab2f10a02f1
Goyal11 <94910258+Goyal11@users.noreply.github.com>
2022-06-02 16:27:38 +0530

    Add files via upload

---
+++ Ineuron_Assissment6.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Q1.
+
+When we use something illegal in a string, we can insert them in a string using an Escape Character.
+
+
+# In[ ]:
+
+
+#Q2.
+
+\n for New line.
+
+\t for Tab.
+
+
+# In[ ]:
+
+
+#Q3.
+
+we can \\ to insert Backlash in a String.
+
+
+# In[ ]:
+
+
+#Q4.
+
+ "Howl's moving castle" is correct because we enclosed a string into Double quotes and single quotes is not used as a escape 
+    character here.
+
+
+# In[ ]:
+
+
+#Q5.
+
+We can enclosed a  string in """ """ if we don't want to use \n.
+
+
+# In[1]:
+
+
+#Q6.
+
+'Hello, world!'[1]
+
+
+# In[2]:
+
+
+#Q6.
+
+'Hello, world!'[0:5]
+
+
+# In[3]:
+
+
+#Q6.
+'Hello, world!'[:5]
+
+
+# In[4]:
+
+
+#Q6.
+
+'Hello, world!'[3:]
+
+
+# In[5]:
+
+
+#Q7.
+
+'Hello'.upper()
+
+
+# In[6]:
+
+
+#Q7.
+
+'Hello'.upper().isupper()
+
+
+# In[7]:
+
+
+#Q7.
+
+'Hello'.upper().lower()
+
+
+# In[9]:
+
+
+#Q8.
+
+'Remember, remember, the fifth of July.'.split()
+
+
+
+# In[10]:
+
+
+#Q8.
+
+'-'.join('There can only one.'.split())
+
+
+# In[ ]:
+
+
+#Q9.
+
+Method to Justify String:
+   string.rjust(length,character)
+   string.ljust(length,character)
+    string.centerjust(length,character).
+
+
+# In[ ]:
+
+
+#Q10.
+
+We can use String.strip(" ") function to elimate Whitespaces in the beginning and End of the String.
+
