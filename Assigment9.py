c2391f14a2823fe313b78e824364263a0abe577a
Goyal11 <94910258+Goyal11@users.noreply.github.com>
2022-06-25 17:43:33 +0530

    Add files via upload

---
+++ Assigment9.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Q1.
+The relative path refers to the path of the file which related to the current working Directory.
+
+
+# In[ ]:
+
+
+#Q2.
+The absloute path refers to the path of the file which stored in your computer somewhere.
+
+
+# In[ ]:
+
+
+#Q3.
+os.getcwd() tells u which is your current directory .
+ Whereas
+os.chdir() set a current directory of your choice ,the path which u have provided.    
+
+
+# In[ ]:
+
+
+#Q4.
+The . signifies the current folder.
+Whereas 
+.. signifies the parent folder.
+
+
+# In[ ]:
+
+
+#Q5.
+C:\bacon\eggs is the directory name and spam.txt is the base name.
+
+
+# In[ ]:
+
+
+#Q6.
+the three “mode” arguments that can be passed to the open() function are:
+    1. write mode
+    2. read mode
+    3. append mode.
+
+
+# In[ ]:
+
+
+#Q7.
+if an existing file is opened in write mode than file will open and all the previous data as been erased and can be overwritten.
+
+
+# In[ ]:
+
+
+#Q8.
+the  read() function will read a entire  file and readlines() function will read a multiple line in a file, the number 
+of lines in a file.
+
+
+# In[ ]:
+
+
+#Q9.
+shelf value resemble a dictionary value, it as a keys and values pair similar to dictionary. 
+
